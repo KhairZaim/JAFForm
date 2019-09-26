50b66b0ed3aa9775554a7b698e05b28b82ff7809
Muhamad Khair Zaim Arfahshad <khair.zaim.arfahshad@simedarby.com>
2019-09-26 16:19:22 +0800

    comment workflow

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,7 +1,7 @@
 import * as functions from 'firebase-functions';
 
-import { Pdf as testPdf3Middleware } from './pdf';
+import { Pdf as testPdf4Middleware } from './pdf';
 
-export const Pdf3 = functions
+export const Pdf = functions
   .runWith({ memory: '512MB' }) /* to launch headless chrome and get pdf */
-  .https.onRequest(testPdf3Middleware);
+  .https.onRequest(testPdf4Middleware);
--- functions/src/workflow/tmb-segmentid/tmb-segmentId-wf.ts
+++ functions/src/workflow/tmb-segmentid/tmb-segmentId-wf.ts
@@ -1,23 +1,23 @@
-import { MappedData, WorkflowField } from '@digitaldealers/typings/safety/mapped-data';
-import { NextFunction, Response } from 'express';
-import { WorkflowRequest } from '../../../interfaces/request.interface';
-import { submitted2new } from './status-processor';
+// import { MappedData, WorkflowField } from '@digitaldealers/typings/safety/mapped-data';
+// import { NextFunction, Response } from 'express';
+// import { WorkflowRequest } from '../../../interfaces/request.interface';
+// import { submitted2new } from './status-processor';
 
-export function tmbRemovSvcSegmentId(req: WorkflowRequest, res: Response, next: NextFunction) {
-    const workflowData: MappedData<WorkflowField> = req.processedBody;
+// export function tmbRemovSvcSegmentId(req: WorkflowRequest, res: Response, next: NextFunction) {
+//     const workflowData: MappedData<WorkflowField> = req.processedBody;
 
-    try {
-        let result: MappedData<WorkflowField>;
-        switch (workflowData.status) {
-            case 'submitted':
-                result = submitted2new(workflowData);
-                break;
-            default:
-                result = workflowData;
-        }
-        req.processedBody = result;
-        return next();
-    } catch (e) {
-        return next(e);
-    }
-}
+//     try {
+//         let result: MappedData<WorkflowField>;
+//         switch (workflowData.status) {
+//             case 'submitted':
+//                 result = submitted2new(workflowData);
+//                 break;
+//             default:
+//                 result = workflowData;
+//         }
+//         req.processedBody = result;
+//         return next();
+//     } catch (e) {
+//         return next(e);
+//     }
+// }
